3c134419a01497796ff3d4b86b76a4e64839bf78
leonardoaguirre <62442754+leonardoaguirre@users.noreply.github.com>
2022-02-23 15:48:56 -0300

    Create all packages in project

---
+++ app/src/main/java/com/dio/portifolio_github_app/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dio.portifolio_github_app.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dio.portifolio_github_app.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
