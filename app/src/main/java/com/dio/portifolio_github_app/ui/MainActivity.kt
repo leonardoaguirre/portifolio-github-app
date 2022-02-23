67c8bfdcf8631b1d44102cd80a7eae8c498a85ae
leonardoaguirre <62442754+leonardoaguirre@users.noreply.github.com>
2022-02-23 16:01:41 -0300

    Create App toolbar with search component

--- app/src/main/java/com/dio/portifolio_github_app/ui/MainActivity.kt
+++ app/src/main/java/com/dio/portifolio_github_app/ui/MainActivity.kt
@@ -2,11 +2,32 @@ package com.dio.portifolio_github_app.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.SearchView
 import com.dio.portifolio_github_app.R
+import com.dio.portifolio_github_app.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
+    private val binding by  lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        val searchView =  menu?.findItem(R.id.action_search)?.actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onQueryTextSubmit(p0: String?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onQueryTextChange(p0: String?): Boolean {
+        TODO("Not yet implemented")
     }
 }
